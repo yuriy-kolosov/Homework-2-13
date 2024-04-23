644fc194d1f29f60141812d1a441ef219a41b08f
yuriy-kolosov <yuriy.v.kolosov@gmail.com>
2024-04-23 22:33:34 +0300

    Домашнее задание 2-13. Тестирование - начальный коммит

---
+++ src/main/java/pro/sky/homework213/JavaSkyProHomework213Application.java
@@ -0,0 +1,13 @@
+package pro.sky.homework213;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSkyProHomework213Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSkyProHomework213Application.class, args);
+	}
+
+}
